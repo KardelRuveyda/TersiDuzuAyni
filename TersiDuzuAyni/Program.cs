b0fd3658f9e13911ee3abf415783a827f48bc5ce
Kardel Rüveyda Çetin <ruveydakardelcetin@gmail.com>
2020-02-03 13:09:57 +0300

    Add project files.

---
+++ TersiDuzuAyni/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace TersiDuzuAyni
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 36500; i++)
+            {
+                Console.WriteLine("Tersi düzü aynı gün: " + DateTime.Today.AddDays(i).ToString("ddMmyyyy"));
+            }
+            Console.ReadLine();
+        }
+       
+
+        public static string Tersi (DateTime dateTime)
+        {
+            var day = dateTime.ToString("ddMMyyyy");
+            char[] reverse = day.ToArray();
+            Array.Reverse(reverse);
+            var a = new string(reverse);
+            return a; 
+        }
+
+        public static bool Kontrol(DateTime dateTime)
+        {
+            string day = dateTime.ToString("ddMmyyyy");
+            var a = day == Tersi(dateTime) ? true : false;
+            return a;
+        }
+        
+     }
+
+}
